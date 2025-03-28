4f206f8df4626c5b8ed20652cfc6ad7c3f7e81d8
xpress4t <emmanuelcm2899@gmail.com>
2025-03-28 21:31:48 +0100

    create Input component

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,27 @@
+import MuiInput from "@mui/material/TextField";
+
+const Input = ({
+  id,
+  label,
+  variant,
+  placeholder,
+  type = "text",
+  sx,
+  onChange,
+  color,
+}) => {
+  return (
+    <MuiInput
+      sx={sx}
+      id={id}
+      label={label}
+      variant={variant}
+      placeholder={placeholder}
+      type={type}
+      onChange={onChange}
+      color={color}
+    />
+  );
+};
+
+export default Input;
