5f5d448b1b01a41d2a19982ee7f2e87c0ae4dd32
xpress4t <emmanuelcm2899@gmail.com>
2025-04-03 20:28:27 +0200

    add test function

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,7 +2,23 @@ import { Box, Link, Typography } from "@mui/material";
 import Button from "../components/Button";
 import Input from "../components/Input";
 
+const test = async () => {
+  const url = "http://localhost/backend/login/";
+  const payload = { email:"pepitoLosPalotes@gmail.com", password:"123456789" };
+  const result = await fetch(url, {
+    body: JSON.stringify(payload),
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await result.json();
+  console.log(data);
+};
+
 export default function Home() {
+  test();
+
   return (
     <Box
       sx={{
@@ -33,7 +49,9 @@ export default function Home() {
       <Box sx={{ display: "flex", flexDirection: "column", width: 300 }}>
         {/* Si hago solo una etiqueta no funcionará ya que estoy esperando que el texto del botón venga a través de children, pero en Home lo estoy pasando como text="Login", lo cual no se está renderizando porque text no es una prop reconocida dentro de Button.
         Asi que paso Login como texto entre la etiqueta de apertura y cierre */}
-        <Button variant="contained" color="primary">Login</Button>
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
       </Box>
 
       <Box>
