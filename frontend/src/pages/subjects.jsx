d4f4c3b42a1187de6688e59dc3f47d89b84018be
xpress4t <emmanuelcm2899@gmail.com>
2025-04-16 20:42:14 +0200

    create delete function - add SubjectModalDelete component

--- frontend/src/pages/subjects.jsx
+++ frontend/src/pages/subjects.jsx
@@ -3,14 +3,17 @@ import { Box, Typography } from "@mui/material";
 import Button from "../components/Button";
 import SubjectTable from "../components/SubjectTable";
 import SubjectModal from "../components/SubjectModal";
+import SubjectModalDelete from "../components/SubjectModalDelete";
 
 const SubjectsPage = () => {
   const [subjects, setSubjects] = useState([]);
-  const [subject, setSubject] = useState(null);
+  const [subjectToEdit, setSubjectToEdit] = useState(null);
+  const [subjectToDelete, setSubjectToDelete] = useState(null);
 
-  const handleOpen = (s) => setSubject(s);
-  const handleClose = () => setSubject(null);
-  const handleSubmit = (e) => {
+  const handleOpenEdit = (s) => setSubjectToEdit(s);
+  const handleCloseEdit = () => setSubjectToEdit(null);
+
+  const handleEdit = (e) => {
     e.preventDefault();
     const id = e.target.subjectId.value;
     const name = e.target.subjectName.value;
@@ -25,7 +28,15 @@ const SubjectsPage = () => {
         return [...prev, { id: lastItem.id + 1, name }];
       });
     }
-    handleClose();
+    handleCloseEdit();
+  };
+
+  const handleOpenDelete = (id) => setSubjectToDelete(id);
+  const handleCloseDelete = () => setSubjectToDelete(null);
+
+  const handleDelete = (id) => {
+    setSubjects((prev) => prev.filter((s) => s.id !== id));
+    handleCloseDelete();
   };
 
   useEffect(() => {
@@ -43,16 +54,25 @@ const SubjectsPage = () => {
           variant="contained"
           color="success"
           size="small"
-          onClick={() => handleOpen({ id: null, name: "" })}
+          onClick={() => handleOpenEdit({ id: null, name: "" })}
         >
           Create
         </Button>
       </Box>
-      <SubjectTable subjects={subjects} onEdit={handleOpen} />
+      <SubjectTable
+        subjects={subjects}
+        onEdit={handleOpenEdit}
+        onDelete={handleOpenDelete}
+      />
       <SubjectModal
-        subject={subject}
-        onClose={handleClose}
-        onSubmit={handleSubmit}
+        subject={subjectToEdit}
+        onClose={handleCloseEdit}
+        onEdit={handleEdit}
+      />
+      <SubjectModalDelete
+        subject={subjectToDelete}
+        onClose={handleCloseDelete}
+        onDelete={handleDelete}
       />
     </Box>
   );
