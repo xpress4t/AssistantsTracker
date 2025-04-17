f0209508d6da463dc267be0dc5d2dd17aa35d795
xpress4t <emmanuelcm2899@gmail.com>
2025-04-17 23:25:17 +0200

    add tools prop

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,6 +1,7 @@
 import * as subjects from "./subjects";
 import * as users from "./users";
+import * as tools from "./tools";
 
-const api = { subjects, users };
+const api = { subjects, users, tools };
 
 export default api;
