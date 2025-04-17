02e59f24b64e958e50d73622962d72c79f0ad4e4
xpress4t <emmanuelcm2899@gmail.com>
2025-04-17 15:10:08 +0200

    create UserTable component

---
+++ frontend/src/components/UserTable.jsx
@@ -0,0 +1,63 @@
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material";
+
+const UserTable = ({ users }) => {
+  return (
+    <TableContainer>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell align="center">Nombres</TableCell>
+            <TableCell align="center">Apellidos</TableCell>
+            <TableCell align="center">Foto</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">Rol</TableCell>
+            <TableCell align="right">Acciones</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((row) => (
+            <TableRow
+              key={row.name}
+
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="center">{row.name}</TableCell>
+              <TableCell align="center">{row.lastname}</TableCell>
+              <TableCell align="center">{row.photo}</TableCell>
+              <TableCell align="center">{row.email}</TableCell>
+              <TableCell align="center">{row.rol}</TableCell>
+              <TableCell align="right">
+                <ButtonGroup
+                  variant="contained"
+                  aria-label="Basic button group"
+                >
+                  <Button color="info" size="small">
+                    Edit
+                  </Button>
+                  <Button color="error" size="small">
+                    Delete
+                  </Button>
+                </ButtonGroup>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default UserTable;
