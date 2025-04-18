d775dd0fb7dc0417044cf4b80014e9d3ebae4d56
xpress4t <emmanuelcm2899@gmail.com>
2025-04-18 14:07:48 +0200

    add courses prop

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,7 +1,8 @@
 import * as subjects from "./subjects";
 import * as users from "./users";
 import * as tools from "./tools";
+import * as courses from "./courses";
 
-const api = { subjects, users, tools };
+const api = { subjects, users, tools, courses };
 
 export default api;
