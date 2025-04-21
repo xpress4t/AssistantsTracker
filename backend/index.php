7b5e64d7eb0c55432cfa1a96f2de5cc2d6d13f1b
xpress4t <emmanuelcm2899@gmail.com>
2025-04-21 20:01:34 +0200

    edit email

--- backend/index.php
+++ backend/index.php
@@ -1,8 +1,7 @@
 <?php
-    require_once("connection.php");
+require_once("connection.php");
 
-    $result = getUserByEmail("pepitoLosPalotes@gmail.com");
+$result = getUserByEmail("emmanuel@gmail.com");
 
-    echo json_encode($result);
-    exit;
-?>
+echo json_encode($result);
+exit;
