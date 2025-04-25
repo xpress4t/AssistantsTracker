0beeefa6d31d95061c73f11e6ab51833869cca9b
xpress4t <emmanuelcm2899@gmail.com>
2025-04-25 21:12:37 +0200

    add new subjects

--- frontend/src/services/subjects.js
+++ frontend/src/services/subjects.js
@@ -4,6 +4,12 @@ export const getSubjects = async () => {
       resolve([
         { id: 1, name: "Matemáticas" },
         { id: 2, name: "Física" },
+        { id: 3, name: "Quimica" },
+        { id: 4, name: "Educacion Fisica" },
+        { id: 5, name: "Inglés" },
+        { id: 6, name: "FOL" },
+        { id: 7, name: "Marcas" },
+        { id: 8, name: "Lengua" },
       ]);
     }, 200);
   });
