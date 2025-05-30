2805c1ad3dc3c64e9967ca56b20280f214977908
xpress4t <emmanuelcm2899@gmail.com>
2025-05-30 15:24:06 +0200

    create AttendanceTable component

--- frontend/src/components/AttendanceTable.jsx
+++ frontend/src/components/AttendanceTable.jsx
@@ -5,9 +5,18 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  Button,
 } from "@mui/material";
+import AttendanceButtons from "./AttendanceButtons";
 
-const AttendanceTable = ({ history = [], subjects, students }) => (
+const AttendanceTable = ({
+  history = [],
+  onHistorySet,
+  onHistoryUpdate,
+  subjects,
+  students,
+  onEdit,
+}) => (
   <TableContainer>
     <Table sx={{ minWidth: 650 }} aria-label="attendance table">
       <TableHead>
@@ -21,20 +30,46 @@ const AttendanceTable = ({ history = [], subjects, students }) => (
 
       <TableBody>
         {history.map((row) => {
-          const student = students.find((s) => s.id === row.studentId);
+          const student = students.find((s) => s.id === row.userId);
           const subject = subjects.find((s) => s.id === row.subjectId);
 
+          const getHandler = () => {
+            if (row.value !== undefined) {
+              return onHistoryUpdate;
+            }
+
+            return onHistorySet;
+          };
+
+          const handleClick = getHandler();
+
           return (
-            <TableRow
-              key={row.id}
-             
-            >
+            <TableRow key={row.attendanceId}>
               <TableCell sx={{ maxWidth: "100px" }}>{row.date}</TableCell>
               <TableCell>
                 {student?.name} {student?.lastname}
               </TableCell>
               <TableCell>{subject?.name}</TableCell>
-              <TableCell align="right">{row.status}</TableCell>
+              <TableCell align="right">
+                {/* <AttendanceValue value={row.value} /> */}
+                <AttendanceButtons
+                  onAbsentsClick={() => {
+                    handleClick(row.userId, row.subjectId, false);
+                  }}
+                  onAssistClick={() => {
+                    handleClick(row.userId, row.subjectId, true);
+                  }}
+                  value={row.value}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{ m: 2 }}
+                  onClick={() => onEdit(row)}
+                >
+                  Editar Asistencia
+                </Button>
+              </TableCell>
             </TableRow>
           );
         })}
