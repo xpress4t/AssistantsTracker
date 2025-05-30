cbebd03ddb90ade8eab0cc07118815500ebc9816
xpress4t <emmanuelcm2899@gmail.com>
2025-05-30 15:24:30 +0200

    create attendance service

--- frontend/src/services/attendance.js
+++ frontend/src/services/attendance.js
@@ -1,39 +1,6 @@
 import { API_URL, attendance } from "./constants";
 
 export const getAttendance = async (filters) => {
-  console.log("Fetching attendance with filters:", filters);
-
-  return [
-    {
-      id: "attendance1",
-      studentId: "2",
-      subjectId: "2",
-      date: "22/05/2025",
-      status: "absent",
-    },
-    {
-      id: "attendance2",
-      studentId: "3",
-      subjectId: "1",
-      date: "22/05/2025",
-      status: "present",
-    },
-    {
-      id: "attendance3",
-      studentId: "6",
-      subjectId: "3",
-      date: "23/05/2025",
-      status: "excused",
-    },
-    {
-      id: "attendance4",
-      studentId: "17",
-      subjectId: "4",
-      date: "24/05/2025",
-      status: "present",
-    },
-  ];
-
   const params = new URLSearchParams(filters);
   let url = `${API_URL}${attendance}`;
 
@@ -50,4 +17,39 @@ export const getAttendance = async (filters) => {
   }
   const data = await res.json();
   return data;
+
+  // return [
+  //   {
+  //     id: "attendance1",
+  //     studentId: "2",
+  //     subjectId: "2",
+  //     date: "22/05/2025",
+  //     status: "absent",
+  //   },
+  //   {
+  //     id: "attendance2",
+  //     studentId: "3",
+  //     subjectId: "1",
+  //     date: "22/05/2025",
+  //     status: "present",
+  //   },
+  //   {
+  //     id: "attendance3",
+  //     studentId: "6",
+  //     subjectId: "3",
+  //     date: "23/05/2025",
+  //     status: "excused",
+  //   },
+  //   {
+  //     id: "attendance4",
+  //     studentId: "17",
+  //     subjectId: "4",
+  //     date: "24/05/2025",
+  //     status: "present",
+  //   },
+  // ];
 };
+
+export const putAttendance = async () => {};
+
+export const postAttendance = async () => {};
