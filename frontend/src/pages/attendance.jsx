c014807eb8b127df83942b1037110fef154612e6
xpress4t <emmanuelcm2899@gmail.com>
2025-05-31 14:29:31 +0200

    update attendance page

--- frontend/src/pages/attendance.jsx
+++ frontend/src/pages/attendance.jsx
@@ -1,13 +1,14 @@
-import { Box, Button } from "@mui/material";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import SearchIcon from "@mui/icons-material/Search";
+import { Box, Button, Toolbar } from "@mui/material";
+import { CalendarIcon } from "@mui/x-date-pickers";
 import Select from "../components/Select";
 import AppBar from "../components/AppBar";
 import Datepicker from "../components/Datepicker";
 import { useState, useEffect } from "react";
 import AttendanceTable from "../components/AttendanceTable";
-import api from "../services";
-import SearchIcon from "@mui/icons-material/Search";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import AttendanceModal from "../components/AttendanceModal";
+import api from "../services";
 
 const AttendancePage = () => {
   // Filtros
@@ -18,34 +19,16 @@ const AttendancePage = () => {
   const [dateTo, setDateTo] = useState("");
   const [loading, setLoading] = useState(false);
   const [history, setHistory] = useState([]);
-
-  const [attendanceToEdit, setAttendanceToEdit] = useState(null);
-  const handleCloseEdit = () => setAttendanceToEdit(null);
-
-  const handleEdit = async (attendance) => {
-    try {
-      if (attendance.id) {
-        await api.attendance.putAttendance(attendance);
-      } else {
-        await api.attendance.postAttendance(attendance);
-      }
-      fetchAttendance();
-      handleCloseEdit();
-    } catch (error) {
-      console.error("Error al guardar la asistencia:", error);
-    }
-  };
-
-  const handleEditClick = (attendance) => {
-    setAttendanceToEdit(attendance);
-  };
+  const [attendanceModalOpen, setAttendanceModalOpen] = useState();
 
   // Datos
   const [subjects, setSubjects] = useState([]);
   const [courses, setCourses] = useState([]);
   const [students, setStudents] = useState([]);
 
-  // 1. Función para obtener todos los cursos
+  const handleCloseEdit = () => setAttendanceModalOpen(false);
+
+  // Función para obtener todos los cursos
   const fetchCourses = async () => {
     setLoading(true);
     const c = await api.courses.getCourses();
@@ -53,7 +36,7 @@ const AttendancePage = () => {
     setLoading(false);
   };
 
-  // 2. Función para obtener todos los estudiantes
+  // Función para obtener todos los estudiantes
   const fetchStudents = async () => {
     setLoading(true);
     const users = await api.users.getUsers(3);
@@ -61,7 +44,7 @@ const AttendancePage = () => {
     setLoading(false);
   };
 
-  // 3. Función para obtener todas las asignaturas
+  // Función para obtener todas las asignaturas
   const fetchSubjects = async () => {
     setLoading(true);
     const s = await api.subjects.getSubjects();
@@ -69,16 +52,20 @@ const AttendancePage = () => {
     setLoading(false);
   };
 
-  // 4. Función para obtener los registros de asistencia (tener en cuenta los filtros)
-  const fetchAttendance = async () => {
+  // Función para obtener los registros de asistencia (tener en cuenta los filtros)
+  const fetchAttendance = async (disableFilters = false) => {
     setLoading(true);
-    const h = await api.attendance.getAttendance({
-      course,
-      student,
-      subject,
-      dateFrom,
-      dateTo,
-    });
+    const h = await api.attendance.getAttendance(
+      disableFilters === true
+        ? {}
+        : {
+            course,
+            student,
+            subject,
+            dateFrom,
+            dateTo,
+          }
+    );
     setHistory(h);
     setLoading(false);
   };
@@ -92,132 +79,162 @@ const AttendancePage = () => {
     setHistory([]);
   };
 
-  const onHistorySet = async (userId, subjectId, value) => {
-    console.log("Creating attendance record:", { userId, subjectId, value });
+  const onHistoryUpdate = async (attendanceId, value) => {
+    setLoading(true);
+    const attendance = { attendanceId, value };
+    const filters = { course, student, subject, dateFrom, dateTo };
+    const res = await api.attendance.putAttendance(attendance, filters);
+    setHistory(res);
+    setLoading(false);
   };
 
-  const onHistoryUpdate = async (userId, subjectId, value) => {
-    console.log("Updating attendance record:", { userId, subjectId, value });
+  const onAttendanceCreate = async (records) => {
+    setHistory(records);
   };
 
   useEffect(() => {
-    // 5. Obtener cursos estudiantes, asignaturas y asistencias
+    // Obtener cursos estudiantes, asignaturas y asistencias
     fetchCourses();
     fetchStudents();
     fetchSubjects();
+    fetchAttendance();
   }, []);
-
-  // 6. useEffect escuchando los filtros para pedir el historial de asistencia filtrado
-  // Aplicar debounce
 
   return (
     <>
       <AppBar title="Attendance" />
-      <Box sx={{ m: 2 }}>
-        <Select
-          id="course"
-          name="course"
-          label="Course"
-          options={courses}
-          value={course}
-          width="200px"
-          onClear={() => {
-            setCourse("");
-          }}
-          onChange={(e) => setCourse(e.target.value)}
-          getOptionLabel={(opt) => opt.name}
-        />
-        <Select
-          id="subject"
-          name="subject"
-          label="Subject"
-          options={subjects}
-          value={subject}
-          width="200px"
-          onClear={() => {
-            setSubject("");
-          }}
-          onChange={(e) => setSubject(e.target.value)}
-          getOptionLabel={(opt) => opt.name}
-        />
-        <Select
-          id="students"
-          name="students"
-          label="Student"
-          options={students.filter((u) => u.role === "3")}
-          value={student}
-          width="200px"
-          onClear={() => {
-            setStudent("");
-          }}
-          onChange={(e) => setStudent(e.target.value)}
-          getOptionLabel={(opt) => opt.name}
-        />
+      <Toolbar
+        sx={{
+          alignItems: "flex-start",
+          display: "flex",
+          mt: 1,
+          px: { xs: 0, sm: 0, md: 1, lg: 1, xl: 1 },
+        }}
+      >
+        <Box sx={{ flex: 1, height: "100%" }}>
+          <Select
+            id="course"
+            name="course"
+            label="Course"
+            options={courses}
+            value={course}
+            width="200px"
+            onClear={() => {
+              setCourse("");
+            }}
+            onChange={(e) => setCourse(e.target.value)}
+            getOptionLabel={(opt) => opt.name}
+            size="small"
+          />
 
-        <Datepicker
-          defaultValue={null}
-          label="Desde"
-          value={dateFrom}
-          onClear={() => {
-            setDateFrom(null);
-          }}
-          onChange={(df) => {
-            setDateFrom(df);
-          }}
-          clearable={true}
-        />
+          <Select
+            id="subject"
+            name="subject"
+            label="Subject"
+            options={subjects}
+            value={subject}
+            width="200px"
+            onClear={() => {
+              setSubject("");
+            }}
+            onChange={(e) => setSubject(e.target.value)}
+            getOptionLabel={(opt) => opt.name}
+            size="small"
+          />
 
-        <Datepicker
-          label="Hasta"
-          value={dateTo}
-          onClear={() => {
-            setDateTo(null);
-          }}
-          onChange={(dt) => setDateTo(dt)}
-          clearable={true}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={fetchAttendance}
-          sx={{ m: 2 }}
-        >
-          <SearchIcon />
-          Buscar
-        </Button>
+          <Select
+            id="students"
+            name="students"
+            label="Student"
+            options={students.filter((u) => u.role === "3")}
+            value={student}
+            width="200px"
+            onClear={() => {
+              setStudent("");
+            }}
+            onChange={(e) => setStudent(e.target.value)}
+            getOptionLabel={(opt) => opt.name}
+            size="small"
+          />
 
-        <Button
-          variant="contained"
-          color="error"
-          onClick={clearData}
-          sx={{ m: 2 }}
-        >
-          <DeleteForeverIcon />
-          Limpiar
-        </Button>
+          <Datepicker
+            defaultValue={null}
+            label="Desde"
+            value={dateFrom}
+            onClear={() => {
+              setDateFrom(null);
+            }}
+            onChange={(df) => {
+              setDateFrom(df);
+            }}
+            clearable={true}
+            size="small"
+          />
 
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => setAttendanceToEdit({})}
-          sx={{ m: 2 }}
-        >
-          Crear Asistencia
-        </Button>
-      </Box>
+          <Datepicker
+            label="Hasta"
+            value={dateTo}
+            onClear={() => {
+              setDateTo(null);
+            }}
+            onChange={(dt) => setDateTo(dt)}
+            clearable={true}
+            size="small"
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={fetchAttendance}
+            sx={{ m: 1 }}
+            startIcon={<SearchIcon />}
+          >
+            Buscar
+          </Button>
+
+          <Button
+            variant="contained"
+            color="error"
+            onClick={clearData}
+            sx={{ m: 1 }}
+            startIcon={<DeleteForeverIcon />}
+          >
+            Limpiar
+          </Button>
+        </Box>
+
+        <Box sx={{ height: "100%" }}>
+          <Button
+            startIcon={<CalendarIcon />}
+            variant="contained"
+            sx={{ m: 1 }}
+            onClick={() => {
+              clearData();
+              fetchAttendance(true);
+
+              setAttendanceModalOpen(true);
+            }}
+          >
+            Create
+          </Button>
+        </Box>
+      </Toolbar>
 
       <AttendanceTable
         history={history}
-        onHistorySet={onHistorySet}
-        onHistoryUpdate={onHistoryUpdate}
         subjects={subjects}
         students={students}
-        onEdit={handleEditClick}
+        onClick={(row, value) => onHistoryUpdate(row.attendanceId, value)}
       />
+
       <AttendanceModal
-        attendance={attendanceToEdit}
         onClose={handleCloseEdit}
-        onEdit={handleEdit}
+        onCreate={onAttendanceCreate}
+        subjects={subjects}
+        students={students}
+        courses={courses}
+        open={attendanceModalOpen}
+        existingHistory={history}
       />
     </>
   );
