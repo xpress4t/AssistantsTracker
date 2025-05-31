35746f677d49b5d08a54f435917fff824b613566
xpress4t <emmanuelcm2899@gmail.com>
2025-05-31 14:31:41 +0200

    Improve Alert style in AttendanceValue component

--- frontend/src/components/AttendanceValue.jsx
+++ frontend/src/components/AttendanceValue.jsx
@@ -3,7 +3,11 @@ import Alert from "@mui/material/Alert";
 const AttendanceValue = ({ value }) => {
   const alertType = value === true ? "success" : "error";
   return (
-    <Alert variant="outlined" severity={alertType} sx={{ width: "100px" }}>
+    <Alert
+      variant="outlined"
+      severity={alertType}
+      sx={{ p: 1, width: "100px", "& > *": { p: "0 !important" } }}
+    >
       {value === true ? "Presente" : "Ausente"}
     </Alert>
   );
