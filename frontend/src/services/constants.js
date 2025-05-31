df4b8cf13c27ee89d27b3544c736e6e0cb055f10
xpress4t <emmanuelcm2899@gmail.com>
2025-05-31 14:28:05 +0200

    add attendances constant

--- frontend/src/services/constants.js
+++ frontend/src/services/constants.js
@@ -4,4 +4,4 @@ export const users = "users/";
 export const subjects = "subjects/";
 export const courses = "courses/";
 export const roles = "roles/";
-export const attendance = "attendance/";
+export const attendances = "attendance/";
