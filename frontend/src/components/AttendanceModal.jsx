b4a1c2ca8dc76af575facd168ed37470cd90d3e1
xpress4t <emmanuelcm2899@gmail.com>
2025-05-31 14:30:52 +0200

    update AttendanceModal component

--- frontend/src/components/AttendanceModal.jsx
+++ frontend/src/components/AttendanceModal.jsx
@@ -1,175 +1,165 @@
-import {
-  Modal,
-  TextField,
-  MenuItem,
-  Box,
-  Typography,
-  Button,
-} from "@mui/material";
 import { useState, useEffect } from "react";
-import Datepicker from "../components/Datepicker";
+import { Dialog, Typography, Box, Button, IconButton } from "@mui/material";
 import Select from "../components/Select";
-import api from "../services";
-import { Today } from "@mui/icons-material";
+import Datepicker from "../components/Datepicker";
+import AttendanceTable from "../components/AttendanceTable";
+import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
+import api from "@/services";
+import { formatDate } from "@/utils/dates";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 300,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+const AttendanceModal = ({
+  courses,
+  subjects,
+  students,
+  onClose,
+  onCreate,
+  open,
+  existingHistory,
+}) => {
+  const [courseId, setCourseId] = useState("");
+  const [subjectId, setSubjectId] = useState("");
+  const [date, setDate] = useState(Date.now());
 
-const AttendanceModal = ({ attendance, onClose, onEdit }) => {
-  const [loading, setLoading] = useState(false);
-  const [students, setStudents] = useState([]);
-  const [subjects, setSubjects] = useState([]);
-  const [formData, setFormData] = useState({
-    userId: attendance?.userId || "",
-    subjectId: attendance?.subjectId || "",
-    date: attendance?.date || "",
-    value: attendance?.value || true,
-  });
+  const course = courses.find((c) => c.id === courseId);
+  const subject = subjects.find((s) => s.id === subjectId);
+
+  const courseSubjects = course?.subjects.map((sub) =>
+    subjects.find((s) => s.id === sub.subjectId)
+  );
+
+  let history = [];
+  if (course && subject) {
+    history = course.studentIds.map((studentId) => {
+      const record = existingHistory.find(
+        (r) =>
+          r.userId === studentId &&
+          r.subjectId === subject.id &&
+          formatDate(r.date) === formatDate(date)
+      );
+
+      return {
+        attendanceId: undefined,
+        userId: studentId,
+        subjectId: subject.id,
+        date: new Date(date).toDateString(),
+        value: record?.value,
+        disableButtons: !!record,
+      };
+    });
+  }
+
+  console.log({ history, existingHistory });
+
+  const onCourseSelect = (e) => {
+    setCourseId(e.target.value);
+    setSubjectId("");
+  };
+
+  const onClick = async (row, value) => {
+    const attendance = { ...row, value };
+    const history = await api.attendance.postAttendance(attendance, {});
+    onCreate(history);
+  };
 
   useEffect(() => {
-    if (attendance) {
-      setFormData({
-        userId: attendance.userId || "",
-        subjectId: attendance.subjectId || "",
-        date: attendance.date || "",
-        value: attendance.value ?? true,
-      });
+    if (!open) {
+      onCourseSelect({ target: { value: "" } });
     }
-  }, [attendance]);
-
-  const fetchStudents = async () => {
-    setLoading(true);
-    const u = await api.users.getUsers(3);
-    setStudents(u);
-    setLoading(false);
-  };
-
-  const fetchSubjects = async () => {
-    setLoading(true);
-    const s = await api.subjects.getSubjects();
-    setSubjects(s);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchStudents();
-    fetchSubjects();
-  }, []);
-
-  const handleChange = (name, value) => {
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
+  }, [open]);
 
   return (
-    <Modal
-      open={!!attendance}
+    <Dialog
+      fullScreen
+      open={open}
       onClose={onClose}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
+      sx={{
+        p: 2,
+        "& > .MuiDialog-container > .MuiPaper-root": {
+          display: "flex",
+          height: "100%",
+        },
+      }}
     >
-      <Box sx={style}>
-        {loading && <>CARGANDO</>}
-        {!loading && (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              onEdit(formData);
-            }}
-          >
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              {attendance?.id ? "Editar" : "Crear"} asistencia
-            </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          m: 1,
+          flexDirection: "row",
+        }}
+      >
+        <Typography
+          id="modal-modal-title"
+          variant="h6"
+          component="h2"
+          sx={{ flex: 1 }}
+        >
+          Crear Asistencia
+        </Typography>
 
-            <Select
-              id="student-select"
-              name="userId"
-              label="Estudiante"
-              options={students}
-              value={formData.userId}
-              onChange={(e) => handleChange("userId", e.target.value)}
-              getOptionLabel={(option) => `${option.name} ${option.lastname}`}
-              defaultValue=""
-              multiple={false}
-            />
-
-            <Select
-              id="subject-select"
-              name="subjectId"
-              label="Asignatura"
-              options={subjects}
-              value={formData.subjectId}
-              onChange={(e) => handleChange("subjectId", e.target.value)}
-              getOptionLabel={(option) => option.name}
-              defaultValue=""
-
-              multiple={false}
-            />
-
-            <Datepicker
-              label="Fecha"
-              value={formData.date}
-              onChange={(newValue) => {
-                handleChange(
-                  "date",
-                  newValue ? newValue.format("YYYY-MM-DD") : ""
-                );
-              }}
-             
-              format="YYYY-MM-DD"
-              clearable={true}
-            />
-
-            <Select
-              id="attendance-select"
-              name="value"
-              label="Asistencia"
-              options={[
-                { id: true, name: "Presente" },
-                { id: false, name: "Ausente" },
-              ]}
-              value={formData.value}
-              onChange={(e) => handleChange("value", e.target.value)}
-              getOptionLabel={(option) => option.name}
-              defaultValue={true}
-              width="100%"
-              multiple={false}
-            />
-
-            <Box
-              aria-label="outlined primary button group"
-              sx={{ mt: 4, display: "flex", justifyContent: "space-evenly" }}
-            >
-              <Button
-                color="info"
-                variant="contained"
-                size="medium"
-                type="submit"
-              >
-                Save
-              </Button>
-
-              <Button
-                color="error"
-                variant="contained"
-                size="medium"
-                onClick={onClose}
-              >
-                Close
-              </Button>
-            </Box>
-          </form>
-        )}
+        <IconButton onClick={onClose}>
+          <CancelRoundedIcon />
+        </IconButton>
       </Box>
-    </Modal>
+
+      <div>
+        <Select
+          id="course"
+          name="course"
+          label="Course"
+          options={courses}
+          value={courseId}
+          width="200px"
+          onClear={() => {
+            setCourseId("");
+          }}
+          onChange={onCourseSelect}
+          getOptionLabel={(opt) => opt.name}
+          size="small"
+        />
+
+        <Select
+          id="subject"
+          name="subject"
+          label="Subject"
+          disabled={!courseId}
+          options={courseSubjects}
+          value={subjectId}
+          width="200px"
+          onClear={() => {
+            setSubjectId("");
+          }}
+          onChange={(e) => setSubjectId(e.target.value)}
+          getOptionLabel={(opt) => opt.name}
+          size="small"
+        />
+
+        <Datepicker
+          defaultValue={null}
+          label="Fecha"
+          value={date}
+          onClear={() => {
+            setDate(null);
+          }}
+          onChange={(df) => {
+            setDate(df);
+          }}
+          clearable={true}
+          size="small"
+        />
+      </div>
+
+      {/* Tabla */}
+      <div style={{ flex: 1 }}>
+        <AttendanceTable
+          history={history}
+          students={students}
+          subjects={subjects}
+          onClick={onClick}
+        />
+      </div>
+    </Dialog>
   );
 };
 
