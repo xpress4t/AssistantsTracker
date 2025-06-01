b649aa8186eda982bd22a4a38971d3c766abe30c
xpress4t <emmanuelcm2899@gmail.com>
2025-06-01 16:12:11 +0200

    update API constants

--- frontend/src/services/constants.js
+++ frontend/src/services/constants.js
@@ -1,7 +1,8 @@
-export const API_URL = "http://localhost/backend/";
+export const API_URL = "http://localhost:3000/api/";
 
-export const users = "users/";
-export const subjects = "subjects/";
-export const courses = "courses/";
-export const roles = "roles/";
-export const attendances = "attendance/";
+export const users = "users";
+export const subjects = "subjects";
+export const courses = "courses";
+export const roles = "roles";
+export const attendances = "attendance";
+export const auth = "auth";
