eb62b5fa8a634fb9e456a88f7e68c1c09301893b
xpress4t <emmanuelcm2899@gmail.com>
2025-06-01 16:18:59 +0200

    add API proxy rewrites to Next.js config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost/backend/:path*" + "/index.php",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
