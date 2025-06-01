4adb4f40abe091d2e0167d86bde302cf1dde3b7c
xpress4t <emmanuelcm2899@gmail.com>
2025-06-01 16:11:40 +0200

    add auth prop

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -3,7 +3,8 @@ import * as users from "./users";
 import * as tools from "./tools";
 import * as courses from "./courses";
 import * as attendance from "./attendance";
+import * as auth from "./auth";
 
-const api = { subjects, users, tools, courses, attendance };
+const api = { subjects, users, tools, courses, attendance, auth };
 
 export default api;
